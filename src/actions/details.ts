fc3ba38b5b591ce6175d834c0cf47346bc6198d1
rosellelopio <rosellelopio@gmail.com>
2025-08-08 03:15:02 +0000

    your commit message here

--- src/actions/details.ts
+++ src/actions/details.ts
@@ -2,5 +2,3 @@
 export const someFn = () => {};
 export const getDetailsByRegistrationId = () => {};
 export const updateDetails = () => {};
-// src/actions/details.ts (placeholder)
-export const someFn = () => {};
